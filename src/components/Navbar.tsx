0e8f906f9c2faba26f616ade03075a8771da015a
Archie Onoya <archieonoya13@gmail.com>
2025-12-13 16:46:37 +0800

    fix: padding and text transitions

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,21 +14,21 @@ export default function Navbar() {
     }, []);
 
     return (
-      <div className={`fixed top-0 z-50 w-full h-12 flex justify-between items-center px-5 font-medium transition-colors ${
+      <div className={`fixed top-0 z-50 w-full h-16 px-20 flex justify-between items-center px-5 text-sm font-medium transition-colors ${
         isHome 
-          ? ' text-[#0F253A]' 
+          ? 'text-[#0F253A]' 
           : 'bg-[#282523] text-white'
       }`}>  <div>
             <span className={`alegreya-extrabold ${isHome ? 'text-[#0F253A]' : 'text-white'} text-xl`}>Great</span>
             <span className="alegreya-extrabold text-[#D76D01] text-xl"> Books</span>
         </div>
         <div className="flex gap-5">
-            <a href="#home">Home</a>
-            <a href="#author">Author</a>
-            <a href="#poem">Poem</a>
-            <a href="#context">Context</a>
-            <a href="#summary">Summary</a>
-            <a href="#analysis">Analysis</a>
+            <a href="#home" className="hover:text-[#D76D01] transition-colors duration-200">Home</a>
+            <a href="#author" className="hover:text-[#D76D01] transition-colors duration-200">Author</a>
+            <a href="#poem" className="hover:text-[#D76D01] transition-colors duration-200">Poem</a>
+            <a href="#context" className="hover:text-[#D76D01] transition-colors duration-200">Context</a>
+            <a href="#summary" className="hover:text-[#D76D01] transition-colors duration-200">Summary</a>
+            <a href="#analysis" className="hover:text-[#D76D01] transition-colors duration-200">Analysis</a>
 
         </div>
     </div>
